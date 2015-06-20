eb5805376554e7170cabaa90e154da098ffbf01a
akihiro_0228 <nano.universe.0228@gmail.com>
2015-06-20 16:30:55 +0900

    ajusted camera postion.

--- DonutsFight/Camera.swift
+++ DonutsFight/Camera.swift
@@ -10,6 +10,23 @@ import SpriteKit
 
 class Camera: Node {
 
+    override var position: CGPoint {
+        get {
+            /*
+            let convertPoint = convert.rPoint(super.position)
+            return CGPoint(x: convertPoint.x + 160, y: convertPoint.y + 284)
+            */
+            return convert.rPoint(super.position)
+        }
+        set {
+            /*
+            let newPoint = CGPoint(x: newValue.x - 160, y: newValue.y - 284)
+            super.position = convert.oPoint(newPoint)
+            */
+            super.position = convert.oPoint(newValue)
+        }
+    }
+
     override init() {
         super.init()
 
--- DonutsFight/GameScene.swift
+++ DonutsFight/GameScene.swift
@@ -9,8 +9,14 @@
 import SpriteKit
 
 class GameScene: BaseScene {
+    let world:   World
+    let overlay: Overlay
+    let camera:  Camera
 
     override init(size: CGSize) {
+        self.world   = World()
+        self.overlay = Overlay()
+        self.camera  = Camera()
         super.init(size: size)
 
         setup()
@@ -21,11 +27,35 @@ class GameScene: BaseScene {
     }
 
     func setup() {
+        self.addChild(self.world)
+        self.world.addChild(self.camera)
+        self.addChild(self.overlay)
 
+        // XXX: 仮
+        let eatSeat = SpriteNode()
+        eatSeat.size = CGSize(width: 320, height: 142)
+        eatSeat.position = CGPoint(x: 0, y: 213)
+        eatSeat.color = UIColor.blueColor()
+        self.world.addChild(eatSeat)
+
+        // XXX: 仮
+        let stomachSeat = SpriteNode()
+        stomachSeat.size = CGSize(width: 320, height: 426)
+        stomachSeat.position = CGPoint(x: 0, y: -71)
+        stomachSeat.color = UIColor.brownColor()
+        self.world.addChild(stomachSeat)
     }
 
     override func didMoveToView(view: SKView) {
 
+    }
+
+    // TODO: もっと綺麗に書けるはずだけど、GameJam中なのでゴメンナサイ
+    override func didSimulatePhysics() {
+        let point = self.camera.position
+        self.camera.position = CGPoint(x: point.x - 160, y: point.y - 284)
+        self.centerOnNode(self.camera)
+        self.camera.position = point
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
